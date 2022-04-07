c4848f686747a6dc985efde42fd92cea82b37fda
Phoenix55537 <konradek.2005@wp.pl>
2022-04-07 10:35:25 +0200

    Input Included

--- index.php
+++ index.php
@@ -1,3 +1,31 @@
+<?php
+    $host = "localhost";
+    $dbuser = "root";
+    $dbpass = "";
+    $dbname = "dane";
+
+    $conn = mysqli_connect($host, $dbuser, $dbpass, $dbname) or die("Błąd połączenia");
+
+    $Widok = $_POST['Widok'];
+    echo $Widok;
+    $Ilosc = $_POST['Ilosc'];
+    echo $Ilosc;
+
+    if ($Ilosc>=0) {
+        if ($Ilosc == 0 || $Ilosc <= 40) {
+            $q = "SELECT imie,nazwisko,rok_urodzenia,opis,zdjecie FROM osoby";
+            $result = mysqli_query($conn,$q);
+        }
+        elseif ($Ilosc>0 && $Ilosc<40) {
+           $q = "SELECT imie,nazwisko,rok_urodzenia,opis,zdjecie FROM osoby LIMIT '$Ilosc'";
+           $resutlt = mysqli_query($conn,$q);
+        }
+    };
+    echo "<pre>";
+        var_dump($result);
+    echo "</pre>";
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,6 +36,21 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <main>
+        <div class="form">
+            <form action="" method="post">
+                <span>W jaki sposób ma zostać wyświetlony wynik</span>
+                <select name="Widok" id="">
+                    <option value="Tabela">Tabela</option>
+                    <option value="Lista">Lista</option>
+                    <option value="Karta">Karta</option>
+                </select>
+                <span>Ile wyświetlić rekordów (0 = WSZYSTKIE)</span>
+                <input type="number" name="Ilosc" id="" default="0">
+                <button type="submit">POKAŻ REKORDY</button>
+            </form>
+        </div>
+        <div class="wynik"></div>
+    </main>
 </body>
 </html>
